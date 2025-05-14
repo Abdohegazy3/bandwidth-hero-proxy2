862f68dbdb76455fe34246fea96a5899a7123e53
Abdohegazy3 <93677036+Abdohegazy3@users.noreply.github.com>
2025-05-14 07:08:49 +0300

    Update shouldCompress.js

--- util/shouldCompress.js
+++ util/shouldCompress.js
@@ -1,14 +1,5 @@
-const MIN_COMPRESS_LENGTH = 1024;
-const MIN_TRANSPARENT_COMPRESS_LENGTH = 102400;
-
-function shouldCompress(imageType, size, isTran>
-  return !(
-    !imageType.startsWith("image") ||
-    size === 0 ||
-    (isTransparent && size < MIN_COMPRESS_LENGT>
-    (!isTransparent && (imageType.endsWith("png>
-  );
-}
-
-module.exports = shouldCompress;
-
+module.exports = (contentType, size, isWebp) => {
+  if (!contentType || !contentType.startsWith('image/')) return false;
+  if (size < 10000) return false;
+  return isWebp || contentType.includes('jpeg') || contentType.includes('png');
+};
