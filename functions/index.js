a442ecc94e5bbe81d651a37a378a77f0f4574d4b
Abdohegazy3 <93677036+Abdohegazy3@users.noreply.github.com>
2025-06-02 06:40:41 +0300

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,69 +1,63 @@
-const fetch = require('node-fetch');
-const pick = require('lodash').pick;
-const shouldCompress = require('../util/shouldCompress');
-const compress = require('../util/compress');
-const DEFAULT_QUALITY = 40;
-
+const pick = require("../util/pick"),
+  fetch = require("node-fetch"),
+  shouldCompress = require("../util/shouldCompress"),
+  compress = require("../util/compress"),
+  DEFAULT_QUALITY = 40;
 exports.handler = async (e, t) => {
   let { url: r } = e.queryStringParameters,
     { jpeg: s, bw: o, l: a } = e.queryStringParameters;
-
   if (!r)
-    return { statusCode: 200, body: 'bandwidth-hero-proxy' };
-
+    return { statusCode: 200, body: "Bandwidth Hero Data Compression Service" };
   try {
     r = JSON.parse(r);
   } catch {}
-  Array.isArray(r) && (r = r.join('&url='));
-  r = r.replace(/http:\/\/1\.1\.\d\.\d\/bmi\/(https?:\/\/)?/i, 'http://');
-
+  Array.isArray(r) && (r = r.join("&url=")),
+    (r = r.replace(/http:\/\/1\.1\.\d\.\d\/bmi\/(https?:\/\/)?/i, "http://"));
   let d = !s,
-    n = o != 0,
+    n = 0 != o,
     i = parseInt(a, 10) || 40;
-
   try {
-    let h = {};
-    const response = await fetch(r, {
-      headers: {
-        ...pick(e.headers, ['cookie', 'dnt', 'referer']),
-        'user-agent': 'Bandwidth-Hero Compressor',
-        'x-forwarded-for': e.headers['x-forwarded-for'] || e.ip,
-        'via': '1.1 bandwidth-hero',
-      },
-      method: 'GET',
-    });
-
-    if (!response.ok) {
-      return { statusCode: response.status || 302 };
-    }
-
-    h = Object.fromEntries(response.headers.entries());
-    const c = await response.buffer();
-    const l = h['content-type'] || '';
-    const p = c.length;
-
-    if (!shouldCompress(l, p, d)) {
-      console.log('Bypassing... Size: ', c.length);
+    let h = {},
+      { data: c, type: l } = await fetch(r, {
+        headers: {
+          ...pick(e.headers, ["cookie", "dnt", "referer"]),
+          "user-agent": "Bandwidth-Hero Compressor",
+          "x-forwarded-for": e.headers["x-forwarded-for"] || e.ip,
+          via: "1.1 bandwidth-hero",
+        },
+      }).then(async (e) =>
+        e.ok
+          ? ((h = e.headers),
+            {
+              data: await e.buffer(),
+              type: e.headers.get("content-type") || "",
+            })
+          : { statusCode: e.status || 302 },
+      ),
+      p = c.length;
+    if (!shouldCompress(l, p, d))
+      return (
+        console.log("Bypassing... Size: ", c.length),
+        {
+          statusCode: 200,
+          body: c.toString("base64"),
+          isBase64Encoded: !0,
+          headers: { "content-encoding": "identity", ...h },
+        }
+      );
+    {
+      let { err: u, output: y, headers: g } = await compress(c, d, n, i, p);
+      if (u) throw (console.log("Conversion failed: ", r), u);
+      console.log(`From ${p}, Saved: ${(p - y.length) / p}%`);
+      let $ = y.toString("base64");
       return {
         statusCode: 200,
-        body: c.toString('base64'),
-        isBase64Encoded: true,
-        headers: { 'content-encoding': 'identity', ...h },
+        body: $,
+        isBase64Encoded: !0,
+        headers: { "content-encoding": "identity", ...h, ...g },
       };
     }
-
-    let { err: u, output: y, headers: g } = await compress(c, d, n, i, p);
-    if (u) throw (console.log('Conversion failed: ', r), u);
-
-    console.log(`From ${p}, Saved: ${(p - y.length) / p}%`);
-    let $ = y.toString('base64');
-    return {
-      statusCode: 200,
-      body: $,
-      isBase64Encoded: true,
-      headers: { 'content-encoding': 'identity', ...h, ...g },
-    };
   } catch (f) {
-    return console.error(f), { statusCode: 500, body: f.message || '' };
+    return console.error(f), { statusCode: 500, body: f.message || "" };
   }
 };
